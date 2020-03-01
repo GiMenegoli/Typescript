18fdf1886b7f466608d12290daa4b15f79200574
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-01 18:52:35 -0300

    GM - Aula 03

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,11 +3,14 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes: Negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -20,6 +23,17 @@ class NegociacaoController {
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoesView.update(this._negociacoes);
+        //this._negociacoes.paraArray().length = 0;
+        //apaga o array inteiro
+        //console.log(negociacao);
+        // this._negociacoes.paraArray().forEach(negociacao => {
+
+        //     console.log(negociacao.data);
+        //     console.log(negociacao.quantidade);
+        //     console.log(negociacao.valor);
+        // })
     }
 }
