4c848e9eb38a118d433da2efba5c45dd4fa88346
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-27 15:44:57 -0300

    Aula 08

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,3 +1,4 @@
+import {logarTempoDeExecucao} from '../helpers/decoratos/index';
 export abstract class View<T> {
 
     protected _elemento: JQuery;
@@ -9,13 +10,17 @@ constructor(selector: string, escapar: boolean = false){
     this._escapar = escapar
     
 }
+@logarTempoDeExecucao()
 
 update(model: T): void{
+   
     let template = this.template(model);
     if(this._escapar)
     template = template.replace(/<script>[\s\S]*?<\/script>/, '');
 
     this._elemento.html(template);
+
+   
 }
 
 
